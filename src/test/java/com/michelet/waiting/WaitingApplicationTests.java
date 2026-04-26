c6523827a461f3661fab80d1460c35ea66a415c6
kim-jinyeong <rlawlsdud51@naver.com>
2026-04-27 02:39:07 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/michelet/waiting/WaitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelet.waiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
