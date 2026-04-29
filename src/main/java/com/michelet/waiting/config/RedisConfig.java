001379381ddfe5752e1961aa9d92af41e1331e1c
Jinyoung-Kim96 <dudwlsrla5151@gmail.com>
2026-04-30 02:56:20 +0900

    Revert "feat: RedisConfig bean add"
    This reverts commit 87288b4a94ec29acb9a0b973fe8e741bba940521.

--- src/main/java/com/michelet/waiting/config/RedisConfig.java
+++ src/main/java/com/michelet/waiting/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.michelet.waiting.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,8 +7,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
