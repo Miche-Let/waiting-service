6f523b2c96b19ec34177da17d7f78894c1256b5b
Jinyoung-Kim96 <dudwlsrla5151@gmail.com>
2026-04-29 15:15:55 +0900

    feat: JpaEntity create

---
+++ src/main/java/com/michelet/waiting/infrastructure/persistence/jpa/WaitingJpaEnitty.java
@@ -0,0 +1,70 @@
+package com.michelet.waiting.infrastructure.persistence.jpa;
+
+import com.michelet.waiting.domain.entity.Waiting;
+import com.michelet.waiting.domain.enums.WaitingStatus;
+import com.michelet.waiting.domain.vo.WaitingToken;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Table(
+        name = "p_waiting_queue",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"user_id","restaurant_id"}
+        ))
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WaitingJpaEnitty {
+
+    @Id
+    @Column(name ="waiting_id", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
+    private UUID userId;
+
+    @Column(name = "restaurant_id", nullable = false, columnDefinition = "BINARY(16)")
+    private UUID restaurantId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private WaitingStatus status;
+
+    @Column(name = "queue_token", nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "entered_at", nullable = false, updatable = false)
+    private LocalDateTime enteredAt;
+
+    @Column(name = "activated_at")
+    private LocalDateTime activatedAt;
+
+    // Domain -> JPA
+    public static WaitingJpaEnitty from(Waiting w){
+        WaitingJpaEnitty e = new WaitingJpaEnitty();
+        e.id = w.getId();
+        e.userId = w.getUserId();
+        e.restaurantId = w.getRestaurantId();
+        e.status = w.getStatus();
+        e.token = w.getToken().value();
+        e.enteredAt = w.getEnteredAt();
+        e.activatedAt = w.getActivatedAt();
+        return e;
+    }
+    // JPA -> Domain
+    public Waiting toDomain() {
+        return Waiting.restore(
+                id, userId, restaurantId,
+                WaitingToken.of(token),
+                status, enteredAt, activatedAt
+        );
+    }
+
+
+}
