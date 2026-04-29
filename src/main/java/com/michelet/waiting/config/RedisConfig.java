360b21171eff081e999e2b87157ca688ad63f8a5
Jinyoung-Kim96 <dudwlsrla5151@gmail.com>
2026-04-30 03:15:18 +0900

    feat: RedisConfig bean add

--- src/main/java/com/michelet/waiting/config/RedisConfig.java
+++ src/main/java/com/michelet/waiting/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.michelet.waiting.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -7,6 +8,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
