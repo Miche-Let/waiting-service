e8858d036bc5cf031c5c748ee6a6e2f0572442eb
Jinyoung-Kim96 <dudwlsrla5151@gmail.com>
2026-04-29 10:43:25 +0900

    refector: record name modified

--- src/main/java/com/michelet/waiting/domain/event/WaitingEnteredEvent.java
+++ src/main/java/com/michelet/waiting/domain/event/WaitingEnteredEvent.java
@@ -2,7 +2,7 @@ package com.michelet.waiting.domain.event;
 
 import java.util.UUID;
 
-public record WaitingEnterEvent(
+public record WaitingEnteredEvent(
         UUID waitingId,
         UUID userId,
         UUID restaurantId
