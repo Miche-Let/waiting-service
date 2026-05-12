f0d59a5fb1ec6bbcc67b9f1c0e8c29310dfbac3f
Jinyoung-Kim96 <dudwlsrla5151@gmail.com>
2026-05-12 15:10:19 +0900

    feat: create WaitingOutboxJpaRepo

---
+++ src/main/java/com/michelet/waiting/infrastructure/persistence/jpa/WaitingOutboxJpaRepository.java
@@ -0,0 +1,11 @@
+package com.michelet.waiting.infrastructure.persistence.jpa;
+
+import com.michelet.waiting.domain.enums.OutboxStatus;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WaitingOutboxJpaRepository extends JpaRepository<WaitingOutboxJpaEntity, UUID> {
+    List<WaitingOutboxJpaEntity> findByStatus(OutboxStatus status);
+
+}
